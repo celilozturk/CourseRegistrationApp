3c6dd8e02c939406ecd30cbf6a409bde2b332153
Celil Ozturk <celil.ozturk@windowslive.com>
2024-07-17 10:42:10 +0200

    LoggingBehavior

---
+++ Application/Behaviors/Logging/LoggingBehavior.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Behaviors.Logging;
+public class LoggingBehavior<TRequest, TResponse> (ILogger<LoggingBehavior<TRequest,TResponse>> logger): IPipelineBehavior<TRequest, TResponse>
+{
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"Handling {typeof(TRequest).Name}");
+
+        var response = await next();
+        logger.LogInformation($"Handled {typeof(TResponse).Name}");
+
+        return response;
+
+    }
+}
--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Application.Behaviors.Logging;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,7 @@ public static class DependencyInjection
         services.AddMediatR(config =>
         {
             config.RegisterServicesFromAssembly(assembly);
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
         });
         services.AddAutoMapper(assembly);
         return services;
